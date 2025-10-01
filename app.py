21579a94a8b40ec8b72c093aa67a9cd22d601616
Gary <zhm917@yahoo.com>
2025-09-30 21:08:07 -0400

    added bootstrap

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+Skaters = [
+      {'name': 'Jessy', 'music': 'Classical Symphony'},
+      {'name': 'Maya', 'music': 'Song B'},
+      {'name': 'Franny', 'music': 'Song C'}
+]
+
+
 @app.route('/')
 def home():
-    return "Welcome to the Skating Music App!"
+    return render_template('home.html', skaters=Skaters)
+
+@app.route('/api/skaters')
+def list_skaters():
+     return jsonify(Skaters)
 
 if __name__ == '__main__':
       app.run(debug=True)
-      
